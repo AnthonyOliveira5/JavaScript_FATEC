cec90991bb6353a1bf7d0a8633a2509662ffaf9f
Anthony <anthony.oliveira5@hotmail.com>
2023-08-30 17:29:20 -0300

    adicionando arquivo da primeira aula

---
+++ atividade1/aula1.js
@@ -0,0 +1,5 @@
+const salario = 1375.78
+const aumento = 0.3
+let novosalario = (salario * aumento) + salario
+
+console.log(`Seu salário era R$ ${salario}, com o aumento de 30%, agora é R$ ${novosalario}`)
